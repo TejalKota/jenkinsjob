f7823cda54d5da8216ff5464f906b516178f692c
TejalKota <121676297+TejalKota@users.noreply.github.com>
2023-12-12 20:56:28 -0800

    Update and rename jenkinsj.java to poejenkins.java

---
+++ poejenkins.java
@@ -0,0 +1,48 @@
+class Graph {
+  class Edge {
+    int src, dest;
+  }
+  int vertices, edges;
+  Edge[] edge;
+  Graph(int vertices, int edges) {
+    this.vertices = vertices;
+    this.edges = edges;
+    edge = new Edge[edges];
+    for(int i = 0; i < edges; i++) {
+      edge[i] = new Edge();
+    }
+  }
+  public static void main(String[] args) {
+    int v = 5;
+    int e = 8;
+    Graph g = new Graph(v, e);
+    g.edge[0].src = 1;   
+    g.edge[0].dest = 2;
+
+    g.edge[1].src = 1;   
+    g.edge[1].dest = 3;
+
+    g.edge[2].src = 1;   
+    g.edge[2].dest = 4;
+
+    g.edge[3].src = 2;   
+    g.edge[3].dest = 4;
+
+    g.edge[4].src = 2;   
+    g.edge[4].dest = 5;
+
+    g.edge[5].src = 3;   
+    g.edge[5].dest = 4;
+
+    g.edge[6].src = 3;   
+    g.edge[6].dest = 5;
+
+    g.edge[7].src = 4;   
+    g.edge[7].dest = 5;
+    for(int i = 0; i < e; i++) {
+      System.out.println(g.edge[i].src + " - " + g.edge[i].dest);
+    }
+
+  }
+}
+
